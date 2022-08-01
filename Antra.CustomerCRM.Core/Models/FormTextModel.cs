b07521481b4dd4485269512abc0eac7a1f63d358
Riku Morishima <riku.morishima@gmail.com>
2022-08-01 10:54:14 -0500

    WIP: Attempting to create a partial form to reduce reduncancy of multiple form spaces

---
+++ Antra.CustomerCRM.Core/Models/FormTextModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Antra.CustomerCRM.Core.Models
+{
+    public class FormTextModel
+    {
+        public string Name { get; set; }
+        public string NameOf { get; set; }
+
+        public string Description { get; set; }
+        public string Value { get; set; }
+    }
+}
--- Antra.CustomerCRM.WebMVC/Controllers/CustomerController.cs
+++ Antra.CustomerCRM.WebMVC/Controllers/CustomerController.cs
@@ -1,14 +1,18 @@
 ﻿using Antra.CustomerCRM.Core.Contracts.Service;
+using Antra.CustomerCRM.Core.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Antra.CustomerCRM.WebMVC.Controllers
 {
     public class CustomerController : Controller
     {
         ICustomerServiceAsync customerServiceAsync;
-        public CustomerController(ICustomerServiceAsync customerServiceAsync)
+        IRegionServiceAsync regionServiceAsync;
+        public CustomerController(ICustomerServiceAsync customerServiceAsync, IRegionServiceAsync regionServiceAsync)
         {
             this.customerServiceAsync = customerServiceAsync;
+            this.regionServiceAsync = regionServiceAsync;
         }
 
         public IActionResult Index()
@@ -21,6 +25,24 @@ namespace Antra.CustomerCRM.WebMVC.Controllers
         {
             var result = await customerServiceAsync.GetModelByIdAsync(id);
             return View(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            ViewBag.Regions = new SelectList(await regionServiceAsync.GetAllModelAsync(), "Id", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CustomerModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                await customerServiceAsync.InsertModelAsync(model);
+                return RedirectToAction("Index");
+            }
+            return View(model);
         }
     }
 }
